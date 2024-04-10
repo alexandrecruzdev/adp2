6145b216cab4931c78c33432b281db0a87e05824
alexandrecruzdev <63272180+alexandrecruzdev@users.noreply.github.com>
2024-04-10 14:02:23 -0300

    adicionando fotos

--- src/components/BannerSlide.jsx
+++ src/components/BannerSlide.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Slider from 'react-slick';
 import Banner from '../images/banner.gif';
+import Img1 from '../images/img1.jpeg'
+import Img5 from '../images/img5.jpeg'
+import Img6 from '../images/img6.jpeg'
+import Img8 from '../images/img8.jpeg'
+import Img9 from '../images/img9.jpeg'
+import Img11 from '../images/img11.jpeg'
+import Img12 from '../images/img12.jpeg'
+import Img13 from '../images/img13.jpeg'
+import Img14 from '../images/img14.jpeg'
+import Img15 from '../images/img15.jpeg'
 
-// Importe os estilos do slick carousel
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -24,7 +33,34 @@ const BannerSlider = () => {
           <img src={Banner} alt="Serviços de Mudança Residencial - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
         </div>
         <div className="overflow-hidden">
-          <img src={Banner} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
+          <img src={Img1} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
+        </div>
+        <div className="overflow-hidden">
+          <img src={Img5} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
+        </div>
+        <div className="overflow-hidden">
+          <img src={Img6} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
+        </div>
+        <div className="overflow-hidden">
+          <img src={Img8} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
+        </div>
+        <div className="overflow-hidden">
+          <img src={Img9} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
+        </div>
+        <div className="overflow-hidden">
+          <img src={Img11} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
+        </div>
+        <div className="overflow-hidden">
+          <img src={Img12} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
+        </div>
+        <div className="overflow-hidden">
+          <img src={Img13} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
+        </div>
+        <div className="overflow-hidden">
+          <img src={Img14} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
+        </div>
+        <div className="overflow-hidden">
+          <img src={Img15} alt="Serviços de Mudança Interestadual - Empresa ADP TRANSPORTES E MUDANÇAS" style={{ maxHeight: '600px', width: '100%' }} />
         </div>
       </Slider>
     </div>
